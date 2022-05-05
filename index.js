101b7d7382d216d59a53c9fd67027a0bd8525382
Eduardo Galera <galera.eduardo@gmail.com>
2022-05-04 22:37:16 -0300

    Segundo Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require ("express");
+require("dotenv").config()
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+server.use(express.json())
+
+server.use("/users", require("./users/usersRouter"))
+
+//Error 404
+server.use((req, res, next)=>{
+    let error = new Error("Resource not found")
+    error.status = 404
+    next(error)
+})
+
+
+
+// Manejador de Errores
+server.use((error, req, res, next)=>{
+    if(!error.status){
+        error.status = 500
+    }
+    res.status(error.status).json({estatus: error.status, message: error.message})
+})
+
+
+server.listen(PORT, (err)=>{
+    err?console.log(`Error: ${err}`): console.log(`Servidor en http://localhost: ${PORT}`);;
+})
+
---
+++ users/usersRouter.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+router.get("/");
+
+router.get("/:id")
+
+router.post("/register");
+
+router.post("/login")
+
+router.delete("/:id")
+
+router.post("/forgot-password")
+
+router.get("/reset/:token")
+
+router.post("/reset/:token")
+
+module.exports = router
