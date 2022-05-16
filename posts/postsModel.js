1be3f9912bc2500b3878aa6cccc51a04f00d7475
Eduardo Galera <galera.eduardo@gmail.com>
2022-05-16 16:32:24 -0300

    Posts completos

--- posts/postsModel.js
+++ posts/postsModel.js
@@ -1,12 +1,31 @@
 const pool = require("../db/config")
 
-const newPosts = (posts)=>{
+const getAllPosts = async()=>{
+    const query = "SELECT * FROM posts"
+    try {
+        return await pool.query(query)
+    } catch (error) {
+        error.message = error.code
+    }
+}
+
+
+const newPosts = async(post)=>{
     const query = "INSERT INTO posts SET ?"
     try {
-        return pool.query(query, posts)
+        return await pool.query(query, post)
     } catch (error) {
         error.message= error.code
     }
 }
 
-module.exports = {newPosts}
+const getPostsWith = async(string)=>{
+    const query = ` SELECT * FROM posts WHERE title LIKE "%${string}%"`
+    try {
+        return await pool.query(query)
+    } catch (error) {
+        error.message= error.code
+    }
+}
+
+module.exports = {getAllPosts,newPosts, getPostsWith}
--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -1,8 +1,10 @@
 const router = require("express").Router()
+const { validatorCreatePost } = require("../validators/posts")
 const isAuth = require("../utils/isAuth")
-const {addOnePosts} = require("./postsController")
+const {addOnePosts, listAll} = require("./postsController")
 
-router.post("/", isAuth, addOnePosts)
+router.post("/", isAuth, validatorCreatePost, addOnePosts)
 
+router.get("/", listAll)
 
 module.exports = router
