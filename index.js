a95e72bda0f1af17c38744f269266aa955ac1b8b
Eduardo Galera <galera.eduardo@gmail.com>
2022-05-16 21:12:06 -0300

    prueba heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 require("dotenv").config()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 require("./db/config")
 const cors = require("cors")
 
